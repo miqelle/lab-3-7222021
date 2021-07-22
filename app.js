70993496e203fc9512f8392eb752777577b674a5
miqelle <miqellec3@gmail.com>
2021-07-22 13:04:57 -0500

    working on lab 3 code

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
-/* //Add a question with multiple correct answers 
+//Add a question with multiple correct answers 
 prompt("What city would I like to visit?");
 
 
 //create an array
 let citiesToVisit = ['New Orleans', 'St. Louis', 'San Diego', 'San Francisco'];
 
+//give the user 6 attempts to guess the correct answer
+for (let guesses = 1; guesses < 7; guesses++)
 
- */
-let userPoints = 0;
+
+//guesses end once correct answer guessed
+if (guesses === 'New Orleans')
+
+//display all possible correct answers to the user
+alert("These are the cities I want to visit: New Orleans, St. Louis, San Diego, San Francisco")
+
+//keep track of total number of correct answer
+if ( guesses === 'New York' ||)
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* let userPoints = 0;
 alert('Welcome to my third guessing game ' + userName + "!");
 
 let answerto = prompt('Is my number one place to visit New Orleans?', 'Type yes or no').toLowerCase();
@@ -17,12 +40,8 @@ let placesToGo = ['New Orleans', 'St. Louis', 'San Diego', 'San Francisco'];
 
 for (let i = 0; i < placesToGo.length; i++) {
   console.log(placesToGo[i]);
-}
 if (answer === 'yes' || answer === 'y') {
- 
   userPoints++;
-} else {
-  alert('Wrong! try again');
 }
 alert('You could have guessed either one of these: New Orleans, St.Louis, San Diego, or San Francisco');
-alert('you have ' + userPoints + ' points.');
+alert('you have ' + userPoints + ' points.') */
